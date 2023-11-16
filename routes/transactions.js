4d1e8467cd079ca7f27f33cb7c5b94828ba77390
foosayyy <dankrish26@gmail.com>
2023-11-16 10:04:25 +0530

    Implement logic in frontend to handle responses from backend API calls

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {getTransactions, addTransactions, deleteTransactions} = require('../controllers/transactions');
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
 
 router
-    .route('/')
-    .get(getTransactions)
-    .post(addTransactions);
+  .route('/')
+  .get(getTransactions)
+  .post(addTransaction);
 
 router
-    .route('/:id')
-    .delete(deleteTransactions);
+  .route('/:id')
+  .delete(deleteTransaction);
+
 module.exports = router;
